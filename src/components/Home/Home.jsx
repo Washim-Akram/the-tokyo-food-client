7396eb8d53679de6ad1537e76f196e16e6393ddc
Washim-Akram <muhammadwashimakram@gmail.com>
2023-05-02 18:19:03 +0600

    Added Navbar and Footer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,7 @@ import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
+        <div className='container'>
             <h2>This is Home</h2>
             <Banner></Banner>
         </div>
